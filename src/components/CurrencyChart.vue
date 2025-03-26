6e1f999a5a4383cb9f08ccd29f96118116e8f7f7
Valeria Zaviriukha <Valeria_Zaviriukha@bitbucket.org>
2025-03-26 18:46:58 +0200

    add store

--- src/components/CurrencyChart.vue
+++ src/components/CurrencyChart.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, computed, onMounted } from "vue";
+import { ref, onMounted } from "vue";
 import { useCurrencyStore } from "@/store/currencyStore";
 import ChartModal from "@/components/ChartModal.vue";
 
