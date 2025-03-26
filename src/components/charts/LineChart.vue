c77d0f99516723579792b7ef46b7bd21b8b0399c
Valeria Zaviriukha <Valeria_Zaviriukha@bitbucket.org>
2025-03-26 13:42:02 +0200

    chart settings

---
+++ src/components/charts/LineChart.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { defineProps, watch } from "vue";
+import { Line } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement
+} from "chart.js";
+
+// Регистрируем компоненты ChartJS
+ChartJS.register(Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement);
+
+// Определяем входные параметры
+const props = defineProps({
+  chartData: Object,
+  options: Object
+});
+
+// Автообновление графика при изменении данных
+watch(
+  () => props.chartData,
+  (newData) => {
+    console.log("Chart data updated:", newData);
+  },
+  { deep: true }
+);
+
+</script>
+
+<template>
+  <Line :data="chartData" :options="options" />
+</template>
