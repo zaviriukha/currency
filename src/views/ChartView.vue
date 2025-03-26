97c0b59bff215e42c098df57493256e6b22449d4
Valeria Zaviriukha <Valeria_Zaviriukha@bitbucket.org>
2025-03-26 11:25:01 +0200

    first commit( Currency Converter + routes)

---
+++ src/views/ChartView.vue
@@ -0,0 +1,7 @@
+<script setup>
+import CurrencyChart from '@/components/CurrencyChart.vue'
+</script>
+
+<template>
+  <CurrencyChart />
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import CurrencyConverter from '@/components/CurrencyConverter.vue'
+</script>
+
+<template>
+  <main>
+    <CurrencyConverter />
+  </main>
+</template>
